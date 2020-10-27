6fa46bbea7a0f9bcfb9393627caf6e8ecc8c688b
cyrus8050 <ckmobile8050@gmail.com>
2020-10-27 23:30:37 +0800

    init

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  useFonts,
+  Akronim_400Regular
+} from '@expo-google-fonts/akronim';
+import { AppLoading } from 'expo';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Akronim_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontFamily: 'Akronim_400Regular', fontSize: 40 }}>Open up App.js </Text>
+        <Text style={{ fontSize: 40 }}>Open up App.js </Text>
+
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
